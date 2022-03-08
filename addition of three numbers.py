3bfaa934aef249b3ba4c01d0ff5989c309fa351f
KarthikYalamanchi <88088707+KarthikYalamanchi@users.noreply.github.com>
2022-03-08 15:49:45 +0530

    Create addition of three numbers.py

---
+++ addition of three numbers.py
@@ -0,0 +1,10 @@
+//Arithmetic  operations
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number"))
+sum = a+b+c
+sub = a-b-c
+divide = a/b/c
+print(sum)
+print(sub)
+print(divide)
